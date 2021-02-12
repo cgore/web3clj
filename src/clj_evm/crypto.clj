27d248f23584ee9f482dbbbcfa1e81b0f1e7b064
Christopher Mark Gore <cgore@cgore.com>
2021-02-11 20:51:50 -0600

    Breaking out a load-credentials helper

--- src/clj_evm/crypto.clj
+++ src/clj_evm/crypto.clj
@@ -1,11 +1,14 @@
 (ns clj-evm.crypto
   (:import [org.web3j.crypto Credentials WalletUtils]))
 
+(defn load-credentials [password source-file]
+  (WalletUtils/loadCredentials  password source-file))
+
 (def demo-wallet
-  (WalletUtils/loadCredentials "Password123" "resources/demo-wallet.json"))
+  (load-credentials "Password123" "resources/demo-wallet.json"))
 
 
 (comment
-
+  (.getAddress demo-wallet)
 
   )
