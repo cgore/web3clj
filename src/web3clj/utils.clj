86a44d0eda7b997698b225202aab4030223fd5dc
Christopher Mark Gore <cgore@cgore.com>
2021-02-15 08:49:08 -0600

    All the units defined easy to get to

---
+++ src/web3clj/utils.clj
@@ -0,0 +1,13 @@
+(ns web3clj.utils
+  (:import [org.web3j.utils Convert Convert$Unit]))
+
+(def wei    Convert$Unit/WEI)
+(def kwei   Convert$Unit/KWEI)
+(def mwei   Convert$Unit/MWEI)
+(def gwei   Convert$Unit/GWEI)
+(def szabo  Convert$Unit/SZABO)
+(def finney Convert$Unit/FINNEY)
+(def ether  Convert$Unit/ETHER)
+(def kether Convert$Unit/KETHER)
+(def mether Convert$Unit/METHER)
+(def gether Convert$Unit/GETHER)
