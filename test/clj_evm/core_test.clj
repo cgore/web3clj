6f562de85e83d6e8c79a62984288b128090d2cc1
Christopher Mark Gore <cgore@cgore.com>
2021-02-15 08:24:10 -0600

    renaming the project

--- test/clj_evm/core_test.clj
+++ test/clj_evm/core_test.clj
@@ -1,7 +1,3 @@
-(ns clj-evm.core-test
+(ns web3clj.core-test
   (:require [clojure.test :refer :all]
-            [clj-evm.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+            [web3clj.core :refer :all]))
