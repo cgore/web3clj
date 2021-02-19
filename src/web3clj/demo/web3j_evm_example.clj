1595d2754d1f907bf64604c98fa1618fd7d68397
Christopher Mark Gore <cgore@cgore.com>
2021-02-19 04:00:24 -0600

    Moving out the demo to its own namespace

---
+++ src/web3clj/demo/web3j_evm_example.clj
@@ -0,0 +1,20 @@
+(ns web3clj.demo.web3j-evm-example
+  "The Demo from web3j-evnexample, but redone as Clojure."
+  (:require [web3clj.abi      :as abi]
+            [web3clj.crypto   :as crypto]
+            [web3clj.evm      :as evm]
+            [web3clj.protocol :as protocol]
+            [web3clj.tx       :as tx]
+            [web3clj.utils    :as utils]))
+
+(comment
+  (def conf (evm/configuration (crypto/address crypto/demo-wallet) 10))
+  (def op-tracer (evm/console-debug-tracer))
+  (def embedded-service (evm/embedded-web3j-service conf op-tracer))
+  (def web3j (protocol/web3j-build embedded-service))
+  (println "Starting a simple ETH transfer transaction ...")
+  (def recipient-address "0x2dfBf35bb7c3c0A466A6C48BEBf3eF7576d3C420")
+  (def tx-receipt (tx/send-funds! web3j
+                                  crypto/demo-wallet
+                                  recipient-address
+                                  (utils/ether 1))))
