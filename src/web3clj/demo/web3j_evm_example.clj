9dbdd58090ce8f99b2479b0bdd3dce39becd99b9
Christopher Mark Gore <cgore@cgore.com>
2021-02-19 04:29:01 -0600

    getting and checking the tx receipt

--- src/web3clj/demo/web3j_evm_example.clj
+++ src/web3clj/demo/web3j_evm_example.clj
@@ -1,5 +1,9 @@
 (ns web3clj.demo.web3j-evm-example
-  "The Demo from web3j-evnexample, but redone as Clojure."
+  "The Demo from web3j-evnexample, but redone as Clojure.
+
+  https://github.com/web3j/web3j-evmexample
+
+  https://github.com/web3j/web3j-evmexample/blob/master/src/main/java/org/web3j/web3jevmexample/Demo.java"
   (:require [web3clj.abi      :as abi]
             [web3clj.crypto   :as crypto]
             [web3clj.evm      :as evm]
@@ -9,12 +13,21 @@
 
 (comment
   (def conf (evm/configuration (crypto/address crypto/demo-wallet) 10))
-  (def op-tracer (evm/console-debug-tracer))
-  (def embedded-service (evm/embedded-web3j-service conf op-tracer))
+
+  ;; (def op-tracer (evm/console-debug-tracer))
+  ;; (def embedded-service (evm/embedded-web3j-service conf op-tracer))
+
+  (def passthrough-tracer (evm/passthrough-tracer))
+  (def embedded-service (evm/embedded-web3j-service conf passthrough-tracer))
+
   (def web3j (protocol/web3j-build embedded-service))
   (println "Starting a simple ETH transfer transaction ...")
   (def recipient-address "0x2dfBf35bb7c3c0A466A6C48BEBf3eF7576d3C420")
   (def tx-receipt (tx/send-funds! web3j
                                   crypto/demo-wallet
                                   recipient-address
-                                  (utils/ether 1))))
+                                  (utils/ether 1)))
+  (println "Transfer transaction receipt:" tx-receipt)
+  (if (tx/ok? tx-receipt)
+    (println "Transaction was ok.")
+    (println "Failed transaction.")))
--- src/web3clj/tx.clj
+++ src/web3clj/tx.clj
@@ -1,16 +1,24 @@
 (ns web3clj.tx
   (:require [web3clj.utils :as utils])
-  (:import [org.web3j.tx Transfer]
+  (:import [org.web3j.protocol.core.methods.response TransactionReceipt]
+           [org.web3j.protocol Web3j]
+           [org.web3j.tx Transfer]
            [org.web3j.tx.gas DefaultGasProvider]))
 
 (defn send-funds
-  ([web3j credentials recipient-address amount]
+  ([^Web3j web3j credentials recipient-address amount]
    (send-funds web3j credentials recipient-address
                (utils/amount->value amount)
                (utils/amount->unit  amount)))
-  ([web3j credentials recipient-address value unit]
+  ([^Web3j web3j credentials recipient-address value unit]
    (Transfer/sendFunds web3j credentials recipient-address
                        value unit)))
 
 (defn send-funds! [& rest]
   (.send (apply send-funds rest)))
+
+(defn tx-hash [^TransactionReceipt tx-receipt]
+  (.getTransactionHash tx-receipt))
+
+(defn ok? [^TransactionReceipt tx-receipt]
+  (.isStatusOK tx-receipt))
