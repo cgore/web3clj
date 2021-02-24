335fba7373b664e5e05c36b4357ceac105624202
Christopher Mark Gore <cgore@cgore.com>
2021-02-24 00:53:48 -0600

    Cleanup on the Address creation with our new fns

--- src/web3clj/abi/datatypes.clj
+++ src/web3clj/abi/datatypes.clj
@@ -20,16 +20,12 @@
   Value can be a Uint, BigInteger, or String (hex value).
   Bit size should be an int."
   ([value]
-   (Address.          (cond (string?            value)              value
-                            (int?               value)  (biginteger value)
-                            (= BigInt     (type value)) (biginteger value)
-                            (= BigInteger (type value))             value
-                            (= Uint       (type value))  (.getValue value))))
+   (Address. (cond (string? value)                    value
+                   (not (nil? (->big-integer value))) (->big-integer value)
+                   (= Uint (type value))              (.getValue value))))
   ([bit-size value]
-   (Address. bit-size (cond (int?               value)  (biginteger value)
-                            (= BigInt     (type value)) (biginteger value)
-                            (= BigInteger (type value))             value
-                            (= Uint       (type value))  (.getValue value)))))
+   (Address. bit-size (cond (not (nil? (->big-integer value)))  (->big-integer value)
+                            (= Uint (type value))               (.getValue value)))))
 
 (defn address->uint [^Address address]
   (.toUint address))
