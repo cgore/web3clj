4a1a4c881ce092508c6fd992d5c9c8dd24dd8245
Christopher Mark Gore <cgore@cgore.com>
2021-03-09 17:04:57 -0600

    docstring

--- src/web3clj/utils/numeric.clj
+++ src/web3clj/utils/numeric.clj
@@ -2,5 +2,9 @@
   (:require [web3clj.abi.datatypes :refer [->big-integer]])
   (:import [org.web3j.utils Numeric]))
 
-(defn encode-quantity [value]
+(defn encode-quantity
+  "Encodes a quantity as a hex string.
+
+  Cf. https://github.com/ethereum/wiki/wiki/JSON-RPC#hex-value-encoding"
+  [value]
   (Numeric/encodeQuantity (->big-integer value)))
