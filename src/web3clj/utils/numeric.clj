639d96394742dacae02e6c67d5672c49d7d08586
Christopher Mark Gore <cgore@cgore.com>
2021-03-10 10:28:54 -0600

    Add "0x..." style hex string support to ->big-integer

--- src/web3clj/utils/numeric.clj
+++ src/web3clj/utils/numeric.clj
@@ -4,29 +4,6 @@
            [org.web3j.abi.datatypes Uint]
            [org.web3j.utils Numeric]))
 
-(defn ->big-integer
-  "Converts integers of various types to BigInteger."
-  [n]
-  (cond (= BigInteger (type n))  n
-        (= BigInt (type n))     (biginteger n)
-        (int? n)                (biginteger n)
-        (string? n)             (biginteger n)))
-
-(defn ->non-negative-big-integer
-  "Converts non-negative integers of various types to a Biginteger."
-  [n]
-  (let [v (->big-integer n)]
-    (when (and (not (nil? v))
-               (not (neg? v)))
-      v)))
-
-(defn encode-quantity
-  "Encodes a quantity as a hex string.
-
-  Cf. https://github.com/ethereum/wiki/wiki/JSON-RPC#hex-value-encoding"
-  [value]
-  (Numeric/encodeQuantity (->big-integer value)))
-
 (defn decode-quantity
   "Decodes a hex string."
   [^String string]
@@ -48,3 +25,28 @@
   (and string
        (string? string)
        (Numeric/containsHexPrefix string)))
+
+(defn ->big-integer
+  "Converts integers of various types to BigInteger."
+  [n]
+  (cond (= BigInteger (type n))         n
+        (= BigInt (type n))            (biginteger n)
+        (int? n)                       (biginteger n)
+        (and (string? n)
+             (contains-hex-prefix? n)) (BigInteger. (clean-hex-prefix n) 16)
+        (string? n)                    (biginteger n)))
+
+(defn ->non-negative-big-integer
+  "Converts non-negative integers of various types to a Biginteger."
+  [n]
+  (let [v (->big-integer n)]
+    (when (and (not (nil? v))
+               (not (neg? v)))
+      v)))
+
+(defn encode-quantity
+  "Encodes a quantity as a hex string.
+
+  Cf. https://github.com/ethereum/wiki/wiki/JSON-RPC#hex-value-encoding"
+  [value]
+  (Numeric/encodeQuantity (->big-integer value)))
--- test/web3clj/utils/numeric_test.clj
+++ test/web3clj/utils/numeric_test.clj
@@ -2,47 +2,6 @@
   (:require [web3clj.utils.numeric :as numeric]
             [clojure.test :refer :all]))
 
-(deftest ->big-integer
-  (testing "passes through an existing BigInteger"
-    (let [i (biginteger 12)]
-      (is (= i (numeric/->big-integer i)))))
-  (testing "works with BigInt"
-    (is (= (biginteger 13)
-           (numeric/->big-integer (bigint 13)))))
-  (testing "works with normal ints"
-    (is (= (biginteger 14)
-           (numeric/->big-integer 14))))
-  (testing "works with strings"
-    (is (= (biginteger 15)
-           (numeric/->big-integer "15")))))
-
-(deftest ->non-negative-big-integer
-  (testing "positives are accepted and translated"
-    (is (= (biginteger 12)
-           (numeric/->non-negative-big-integer 12)
-           (numeric/->non-negative-big-integer (bigint 12))
-           (numeric/->non-negative-big-integer (biginteger 12))
-           (numeric/->non-negative-big-integer "12"))))
-  (testing "negatives are rejected and nil is returned"
-    (is (= nil
-           (numeric/->non-negative-big-integer -12)
-           (numeric/->non-negative-big-integer (bigint -12))
-           (numeric/->non-negative-big-integer (biginteger -12))
-           (numeric/->non-negative-big-integer "-12")))))
-
-(deftest encode-quantity
-  (testing "some examples"
-    (are [result input] (= result (numeric/encode-quantity input))
-      "0x0"       0
-      "0xc"       12
-      "0x7b"      123
-      "0x4d2"     1234
-      "0x3039"    12345
-      "0x1e240"   123456
-      "0x12d687"  1234567
-      "0xbc614e"  12345678
-      "0x75bcd15" 123456789)))
-
 (deftest decode-quantity
   (testing "some examples"
     (are [input result] (= result (numeric/decode-quantity input))
@@ -77,3 +36,47 @@
     (is (not (numeric/contains-hex-prefix? "123"))))
   (testing "true case"
     (is (numeric/contains-hex-prefix? "0x123"))))
+
+(deftest ->big-integer
+  (testing "passes through an existing BigInteger"
+    (let [i (biginteger 12)]
+      (is (= i (numeric/->big-integer i)))))
+  (testing "works with BigInt"
+    (is (= (biginteger 13)
+           (numeric/->big-integer (bigint 13)))))
+  (testing "works with normal ints"
+    (is (= (biginteger 14)
+           (numeric/->big-integer 14))))
+  (testing "works with strings"
+    (is (= (biginteger 15)
+           (numeric/->big-integer "15"))))
+  (testing "works with hex strings"
+    (is (= (biginteger 123)
+           (numeric/->big-integer "0x7b")))))
+
+(deftest ->non-negative-big-integer
+  (testing "positives are accepted and translated"
+    (is (= (biginteger 12)
+           (numeric/->non-negative-big-integer 12)
+           (numeric/->non-negative-big-integer (bigint 12))
+           (numeric/->non-negative-big-integer (biginteger 12))
+           (numeric/->non-negative-big-integer "12"))))
+  (testing "negatives are rejected and nil is returned"
+    (is (= nil
+           (numeric/->non-negative-big-integer -12)
+           (numeric/->non-negative-big-integer (bigint -12))
+           (numeric/->non-negative-big-integer (biginteger -12))
+           (numeric/->non-negative-big-integer "-12")))))
+
+(deftest encode-quantity
+  (testing "some examples"
+    (are [result input] (= result (numeric/encode-quantity input))
+      "0x0"       0
+      "0xc"       12
+      "0x7b"      123
+      "0x4d2"     1234
+      "0x3039"    12345
+      "0x1e240"   123456
+      "0x12d687"  1234567
+      "0xbc614e"  12345678
+      "0x75bcd15" 123456789)))
