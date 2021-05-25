49472845f3ff8c81ba2d3b1dbceac837aad14c83
Christopher Mark Gore <cgore@cgore.com>
2021-05-25 11:19:08 -0500

    adding com.cgore to the project

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject web3clj "0.1.0"
+(defproject com.cgore/web3clj "0.1.0"
   :description " A Clojure wrapper around web3j"
   :url "https://github.com/cgore/web3clj"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
@@ -6,5 +6,7 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.web3j/core      "4.8.4"]
                  [org.web3j/web3j-evm "4.8.4"]]
-  :repositories [["jcenter" {:url "https://jcenter.bintray.com/"}]]
+  :repositories [["jcenter" {:url "https://jcenter.bintray.com/"}]
+                 ["releases" {:url "https://repo.clojars.org"
+                              :creds :gpg}]]
   :repl-options {:init-ns web3clj.core})
